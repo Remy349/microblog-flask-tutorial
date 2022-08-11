a4c28266cba6d9f02f9715145f5dc2e0ad3cff00
Remy349 <santoraga15@gmail.com>
2022-08-10 22:19:31 -0600

    Added database integration

--- application.py
+++ application.py
@@ -1,1 +1,7 @@
-from flaskr import app
+from flaskr import app, db
+from flaskr.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_contenxt():
+    return {"db": db, "User": User, "Post": Post}
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "microblog.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
