cc288e7d3eea57bdc56ae44f62d4c9af06f66b51
Remy349 <santoraga15@gmail.com>
2022-08-10 19:12:39 -0600

    Adding web forms (Flask-WTF)

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
 from flaskr import app
+from flask import flash, redirect, render_template, url_for
+from flaskr.forms import LoginForm
+
 
 @app.route("/")
 @app.route("/index")
@@ -19,3 +21,16 @@ def index():
 
     return render_template("index.html", title="Home", user=user,
                            posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for("index"))
+
+    return render_template("forms/login.html", title="Sign In", form=form)
