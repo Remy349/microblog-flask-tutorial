c32e1af9632563bf66240652c2599972a90c57ec
Remy349 <santoraga15@gmail.com>
2022-08-12 21:57:46 -0600

    Added error handling (404 | 500)

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -85,7 +85,7 @@ def user(username):
 
 @app.route("/edit_user", methods=["GET", "POST"])
 def edit_user():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
 
     if form.validate_on_submit():
         current_user.username = form.username.data
