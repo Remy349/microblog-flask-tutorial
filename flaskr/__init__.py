782abbbf0ff8d5fec74862d202021033c0b2e36b
Remy349 <santoraga15@gmail.com>
2022-08-17 15:50:42 -0600

    Added dates and times support

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login.login_view = "login"
 
